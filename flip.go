b673710589641f5184b73ce3c6de36f2ed5b6439
griffindavis02 <davisgriffinp@gmail.com>
2021-10-24 18:03:52 -0400

    Draft license message

--- flip.go
+++ flip.go
@@ -1,3 +1,19 @@
+// Copyright 2021 The eth-bit-flip Authors
+// This file is part of the eth-bit-flip library.
+//
+// The eth-bit-flip libary is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// The eth-bit-flip libary is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the eth-bit-flip library. If not, see <https://www.gnu.org/licenses/>.
+
 package BitFlip
 
 import (
