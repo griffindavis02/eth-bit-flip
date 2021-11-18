14dd22333291a82ca43a78058641a8bc9b1e2485
davisgriffin <70169481+davisgriffin@users.noreply.github.com>
2021-11-17 20:39:51 -0500

    name changes

--- config/config.go
+++ config/config.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License along with
 // the eth-bit-flip library. If not, see <https://www.gnu.org/licenses/>.
 
-package flipcfg
+package config
 
 import (
 	"bufio"
