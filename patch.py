6e71f37cd77ea7245c19d5855322e91297011f55
griffindavis02 <davisgriffinp@gmail.com>
2021-11-21 18:32:51 -0500

    working patch functions

--- patch.py
+++ patch.py
@@ -1,3 +1,73 @@
+import os
 import sys
+import re
 
-print(sys.argv[1])
+dirGeth = re.sub(r'\\', '/', sys.argv[1])
+fileTriggers = {
+    'cmd/geth/maintest.go': ['utils.MetricsInfluxDBOrganizationFlag,', 'app.Flags = append(app.Flags, metricsFlags...)']
+}
+
+patches = {
+    'cmd/geth/maintest.go': ["""
+    flipFlags = []cli.Flag{
+		utils.FlipPath,
+		utils.FlipStart,
+		utils.FlipStop,
+		utils.FlipRestart,
+		utils.FlipType,
+		utils.FlipCounter,
+		utils.FlipIterations,
+		utils.FlipVariables,
+		utils.FlipDuration,
+		utils.FlipTime,
+		utils.FlipRate,
+		utils.FlipRates,
+		utils.FlipPost,
+		utils.FlipHost,
+	}
+    """, '\tapp.Flags = append(app.Flags, flipFlags...)']
+}
+
+def main():
+    """
+    Parse through the go-ethereum source code and insert the necessary function
+    calls
+    """
+    fileNames = list(fileTriggers)
+
+    triggerContent = findTrigger(fileNames[0], fileTriggers[fileNames[0]][0], 0)
+    patch(fileNames[0], triggerContent[1], patches[fileNames[0]][0], triggerContent[0], 1)
+    triggerContent = findTrigger(fileNames[0], fileTriggers[fileNames[0]][1], 0)
+    patch(fileNames[0], triggerContent[1], patches[fileNames[0]][1], triggerContent[0], 0)
+
+def findTrigger(fileName: str, trigger: str, overrides: int) -> (int, list[str]):
+    """
+    Reads the selected file in the geth soruce code and returns the line number
+    of the selected trigger and a list of file lines.
+    """
+    filePath = os.path.join(dirGeth, fileName)
+    print(f'Reading file {filePath}')
+
+    with open(filePath, 'r') as rFile:
+        lines = rFile.readlines()
+        overrideCheck = 0
+        for lineNum, line in enumerate(lines):
+            if trigger in line:
+                if overrideCheck >= overrides:
+                    print('found trigger')
+                    return (lineNum, lines)
+                overrideCheck+=1
+
+def patch(fileName: str, fileContents: list[str], patchStr: str,
+            lineNum: int, offset: int) -> None:
+    """
+    Takes the given file name and its contents, writes the patch into the file
+    at line lineNum + offset
+    """
+    filePath = os.path.join(dirGeth, fileName)
+    print(f'Writing file {filePath}')
+    with open(filePath, 'w') as wFile:
+        fileContents[lineNum+offset+1:lineNum+offset+1] = re.split(r'(\n)', patchStr)
+        wFile.write(''.join(fileContents))
+
+main()
