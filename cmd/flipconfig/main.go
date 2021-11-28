f93087e84cb9b3e39aa5825ed91f1b7237fae15d
griffindavis02 <davisgriffinp@gmail.com>
2021-11-28 17:14:49 -0500

    main config package

--- cmd/flipconfig/main.go
+++ cmd/flipconfig/main.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License along with
 // the eth-bit-flip library. If not, see <https://www.gnu.org/licenses/>.
 
-package config
+package main
 
 import (
 	"bufio"
